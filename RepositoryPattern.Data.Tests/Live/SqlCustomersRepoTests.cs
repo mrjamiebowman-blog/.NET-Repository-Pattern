beedbe1c76244872cc63d9d3c5894c2170ca4260
Jamie Bowman <mrjamiebowman@gmail.com>
2020-07-10 21:01:09 -0400

    unit test running sql repo code

---
+++ RepositoryPattern.Data.Tests/Live/SqlCustomersRepoTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryPattern.Data.Repositories;
+using RepositoryPattern.Data.Repositories.Interfaces;
+using RepositoryPattern.Data.Services;
+using RepositoryPattern.Data.Tests.Repositories;
+using Xunit;
+
+namespace RepositoryPattern.Data.Tests.Live
+{
+    public class SqlCustomersRepoTests
+    {
+        public SqlCustomersRepoTests()
+        {
+
+        }
+
+        [Fact]
+        public async Task GetCustomersAsyncTests()
+        {
+            // arrange
+            var _customerRepo = new SqlCustomersRepository();
+
+            int id = 1;
+
+            // act
+            var customer = await _customerRepo.GetByIdAsync(id);
+        }
+    }
+}
--- RepositoryPattern.Data/Repositories/SqlCustomersRepository.cs
+++ RepositoryPattern.Data/Repositories/SqlCustomersRepository.cs
@@ -20,7 +20,13 @@ namespace RepositoryPattern.Data.Repositories
                     conn.Open();
 
                     var parameters = new DynamicParameters();
-                    //parameters.Add("@PARAM", "");
+                    parameters.Add("@FirstName", model.FirstName);
+                    parameters.Add("@LastName", model.LastName);
+                    parameters.Add("@Email", model.Email);
+                    if (model.BillingAddress.AddressId.HasValue)
+                        parameters.Add("@BillingAddressId", model.BillingAddress.AddressId);
+                    if (model.ShippingAddress.AddressId.HasValue)
+                        parameters.Add("@ShippingAddressId", model.ShippingAddress.AddressId);
 
                     var data = (await conn.QueryAsync<Customer>(StoredProcedures.CreateCustomer, parameters, commandType: CommandType.StoredProcedure)).SingleOrDefault();
                     return data;
@@ -41,7 +47,7 @@ namespace RepositoryPattern.Data.Repositories
                     conn.Open();
 
                     var parameters = new DynamicParameters();
-                    //parameters.Add("@PARAM", "");
+                    parameters.Add("@CustomerId", id);
 
                     await conn.QueryAsync(StoredProcedures.DeleteCustomer, parameters, commandType: CommandType.StoredProcedure);
                 }
@@ -61,7 +67,7 @@ namespace RepositoryPattern.Data.Repositories
                     conn.Open();
 
                     var parameters = new DynamicParameters();
-                    //parameters.Add("@PARAM", "");
+                    parameters.Add("@CustomerId", id);
 
                     var data = (await conn.QueryAsync<Customer>(StoredProcedures.GetCustomer, parameters, commandType: CommandType.StoredProcedure)).SingleOrDefault();
                     return data;
@@ -82,7 +88,12 @@ namespace RepositoryPattern.Data.Repositories
                     conn.Open();
 
                     var parameters = new DynamicParameters();
-                    //parameters.Add("@PARAM", "");
+                    parameters.Add("@FirstName", "");
+                    parameters.Add("@LastName", "");
+                    parameters.Add("@Email", "");
+                    parameters.Add("@City", "");
+                    parameters.Add("@State", "");
+                    parameters.Add("@Country", "");
 
                     var data = (await conn.QueryAsync<Customer>(StoredProcedures.GetCustomers, parameters, commandType: CommandType.StoredProcedure)).ToList();
                     return data;
@@ -103,7 +114,13 @@ namespace RepositoryPattern.Data.Repositories
                     conn.Open();
 
                     var parameters = new DynamicParameters();
-                    //parameters.Add("@PARAM", "");
+                    parameters.Add("@FirstName", model.FirstName);
+                    parameters.Add("@LastName", model.LastName);
+                    parameters.Add("@Email", model.Email);
+                    if (model.BillingAddress.AddressId.HasValue)
+                        parameters.Add("@BillingAddressId", model.BillingAddress.AddressId);
+                    if (model.ShippingAddress.AddressId.HasValue)
+                        parameters.Add("@ShippingAddressId", model.ShippingAddress.AddressId);
 
                     var data = (await conn.QueryAsync<Customer>(StoredProcedures.SaveCustomer, parameters, commandType: CommandType.StoredProcedure)).SingleOrDefault();
                     return data;
